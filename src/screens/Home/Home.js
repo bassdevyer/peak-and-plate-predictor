27a07a048db6d6efb403cbafb52baa2ed16ebce4
Miguel Torres <miguetv17@gmail.com>
2019-06-28 01:39:15 -0500

    WIP

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -20,7 +20,6 @@ export default class Home extends Component {
       platePrefix: '',
       plateSuffix: '',
       dateTime: '',
-      canBeOnRoad: null,
     }
   }
 
@@ -45,81 +44,75 @@ export default class Home extends Component {
   }
 
   checkIfCanBeOnRoad = () => {
-    const dateTime = new Date(this.state.dateTime)
-    const dayOfWeekIndex = dateTime.getDay()
-    const morningStartTime = new Date(this.state.dateTime).setHours(7, 0, 0)
-    const morningEndTime = new Date(this.state.dateTime).setHours(9, 30, 0)
-    const afternoonStartTime = new Date(this.state.dateTime).setHours(16, 0,
-      0)
-    const afternoonEndTime = new Date(this.state.dateTime).setHours(19, 30, 0)
-
-    // Check if it's a working day
-    if (dayOfWeekIndex > 0
-      && dayOfWeekIndex < 6
-      // If so, check if time is between 07:00 to 09:30 or 16:00 to 19:30
-      && ((dateTime >= morningStartTime && dateTime <= morningEndTime)
-        || (dateTime >= afternoonStartTime && dateTime <= afternoonEndTime))) {
-      const plateLastDigit = this.state.plateSuffix.slice(-1)
-      console.log(plateLastDigit)
-      switch (dayOfWeekIndex) {
-        // Monday
-        case 1:
-          if (plateLastDigit === '1' || plateLastDigit === '2') {
-            this.setState({ canBeOnRoad: false })
-          }
-          break
-        // Tuesday
-        case 2:
-          if (plateLastDigit === '3' || plateLastDigit === '4') {
-            this.setState({ canBeOnRoad: false })
-          }
-          break
-        // Wednesday
-        case 3:
-          if (plateLastDigit === '5' || plateLastDigit === '6') {
-            this.setState({ canBeOnRoad: false })
-          }
-          break
-        // Thursday
-        case 4:
-          if (plateLastDigit === '7' || plateLastDigit === '8') {
-            this.setState({ canBeOnRoad: false })
-          }
-          break
-        // Friday
-        case 5:
-          if (plateLastDigit === '9' || plateLastDigit === '0') {
-            this.setState({ canBeOnRoad: false })
-          }
-          break
-        default:
-          this.setState({ canBeOnRoad: true })
-
-      }
-    }else{
-      this.setState({ canBeOnRoad: true })
-    }
-    console.log('canBeOnRoad', this.state.canBeOnRoad)
-  }
-
-  shouldComponentUpdate (nextProps, nextState) {
-    return (this.state.platePrefix !== nextState.platePrefix)
-      || (this.state.plateSuffix !== nextState.plateSuffix)
-      || (this.state.dateTime !== nextState.dateTime)
-      || (this.state.canBeOnRoad !== nextState.canBeOnRoad)
-
-  }
-
-  componentDidUpdate = () => {
-    console.log(this.state)
+    let result = {}
+    const negativeResult = { style: 'danger', label: 'No puede circular' }
+    const positiveResult = { style: 'success', label: 'Puede circular' }
+    const defaultResult = { style: 'default', label: 'Complete los datos' }
     if (this.state.platePrefix.length === 3
       && this.state.plateSuffix.length >= 3
       && this.state.dateTime) {
-      this.checkIfCanBeOnRoad()
+      const dateTime = new Date(this.state.dateTime)
+      const dayOfWeekIndex = dateTime.getDay()
+      const morningStartTime = new Date(this.state.dateTime).setHours(7, 0, 0)
+      const morningEndTime = new Date(this.state.dateTime).setHours(9, 30, 0)
+      const afternoonStartTime = new Date(this.state.dateTime).setHours(16, 0,
+        0)
+      const afternoonEndTime = new Date(this.state.dateTime).setHours(19, 30, 0)
+
+      // Check if it's a working day
+      if (dayOfWeekIndex > 0
+        && dayOfWeekIndex < 6
+        // If so, check if time is between 07:00 to 09:30 or 16:00 to 19:30
+        && ((dateTime >= morningStartTime && dateTime <= morningEndTime)
+          ||
+          (dateTime >= afternoonStartTime && dateTime <= afternoonEndTime))) {
+        const plateLastDigit = this.state.plateSuffix.slice(-1)
+        switch (dayOfWeekIndex) {
+          // Monday
+          case 1:
+            if (plateLastDigit === '1' || plateLastDigit === '2') {
+              result = negativeResult
+            }
+            break
+          // Tuesday
+          case 2:
+            if (plateLastDigit === '3' || plateLastDigit === '4') {
+              result = negativeResult
+            }
+            break
+          // Wednesday
+          case 3:
+            if (plateLastDigit === '5' || plateLastDigit === '6') {
+              result = negativeResult
+            }
+            break
+          // Thursday
+          case 4:
+            if (plateLastDigit === '7' || plateLastDigit === '8') {
+              result = negativeResult
+            }
+            break
+          // Friday
+          case 5:
+            if (plateLastDigit === '9' || plateLastDigit === '0') {
+              result = negativeResult
+            }
+            break
+          default:
+            result = positiveResult
+
+        }
+      } else {
+        result = positiveResult
+      }
+    } else {
+      result = defaultResult
     }
+    return result
   }
 
   render () {
+    const result = this.checkIfCanBeOnRoad()
     return (
       <Panel bsStyle="primary">
         <Panel.Heading>
@@ -158,15 +151,7 @@ export default class Home extends Component {
               </InputGroup>
             </FormGroup>
             <FormGroup>
-              <Label bsStyle={this.state.canBeOnRoad === null
-                ? 'default'
-                : this.state.canBeOnRoad
-                  ? 'success'
-                  : 'danger'}>{this.state.canBeOnRoad === null
-                ? 'Complete los datos'
-                : this.state.canBeOnRoad
-                  ? 'Puede circular'
-                  : 'No puede circular'}</Label>
+              <Label bsStyle={result.style}>{result.label}</Label>
             </FormGroup>
 
           </form>
