11de6bca6b96b6c953d52645946e879415840c90
Miguel Torres <miguetv17@gmail.com>
2019-06-30 21:06:44 -0500

    WIP

--- src/components/PlateSuffixFormControl/PlateSuffixFormControl.js
+++ src/components/PlateSuffixFormControl/PlateSuffixFormControl.js
@@ -1,22 +1,55 @@
 import React from 'react'
 import './style.css'
 import { FormControl } from 'react-bootstrap'
+import PropTypes from 'prop-types'
 
-const PlateSuffixFormControl = (props) => {
-
-  const _onPlateSuffixKeyPress = e => {
-    const regExp = /[0-9]+/g
-    if (!regExp.test(e.key)) {
-      e.preventDefault()
-    }
+const propTypes = {
+    className: PropTypes.string,
+    id: PropTypes.string,
+    type: PropTypes.string,
+    maxLength: PropTypes.number,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+  },
+  defaultProps = {
+    className: 'plateSuffixFormControl',
+    type: 'text',
+    maxLength: 4,
+    onKeyPress: (e) => _onPlateSuffixKeyPress(
+      e),
   }
 
+const _onPlateSuffixKeyPress = e => {
+  const regExp = /[0-9]+/g
+  if (!regExp.test(e.key)) {
+    e.preventDefault()
+  }
+}
+
+const PlateSuffixFormControl = (props) => {
+  const {
+    className,
+    id,
+    maxLength,
+    onChange,
+    onKeyPress,
+    type,
+    value,
+  } = props
+
   return (
-    <FormControl className='plateSuffixFormControl' type="text" maxLength={4}
-                 onKeyPress={(e) => _onPlateSuffixKeyPress(
-                   e)}
-                 {...props}/>
+    <FormControl
+      className={className}
+      id={id}
+      maxLength={maxLength}
+      onChange={onChange}
+      onKeyPress={onKeyPress}
+      type={type}
+      value={value}/>
   )
 }
+
+PlateSuffixFormControl.propTypes = propTypes
+PlateSuffixFormControl.defaultProps = defaultProps
 
 export default PlateSuffixFormControl
