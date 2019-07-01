f6d57ff247eab63200e4263fccab0813048cafed
Miguel Torres <miguetv17@gmail.com>
2019-06-30 20:59:27 -0500

    WIP

--- src/components/PlatePrefixFormControl/PlatePrefixFormControl.test.js
+++ src/components/PlatePrefixFormControl/PlatePrefixFormControl.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PlatePrefixFormControl from '../PlatePrefixFormControl'
 import { create } from 'react-test-renderer'
+import { mount } from 'enzyme/build'
 
 describe('Component default rendering', () => {
   it('Renders component correctly', () => {
@@ -12,45 +13,48 @@ describe('Component default rendering', () => {
   })
 })
 
-describe('Testing props', () => {
-  it('Sets default props', () => {
-    const props = {
-      className: 'platePrefixFormControl',
-      type: 'text',
-      maxLength: 3,
-    }
-    expect(PlatePrefixFormControl.defaultProps.className)
-      .toEqual(props.className)
-    expect(PlatePrefixFormControl.defaultProps.type)
-      .toEqual(props.type)
-    expect(PlatePrefixFormControl.defaultProps.maxLength)
-      .toEqual(props.maxLength)
-    expect(PlatePrefixFormControl.defaultProps.onKeyPress)
-      .toBeDefined()
+describe('Component props', () => {
+  it('checks default props', () => {
+    const DateInputComponent = mount(<PlatePrefixFormControl/>)
+
+    expect(DateInputComponent.prop('className'))
+      .toEqual('platePrefixFormControl')
+    expect(DateInputComponent.prop('type'))
+      .toEqual('text')
+    expect(DateInputComponent.prop('maxLength'))
+      .toEqual(3)
+    expect(DateInputComponent.prop('onKeyPress'))
+      .toBeFunction()
   })
 
-  it('Sets external props', () => {
-    const props = {
-      value: '',
-      onChange: jest.fn(),
-    }
-    const component = create(
-      <PlatePrefixFormControl/>,
-    )
-    expect(PlatePrefixFormControl.props.className)
-      .toEqual(props.className)
-    expect(PlatePrefixFormControl.defaultProps.type)
-      .toEqual(props.type)
-    expect(PlatePrefixFormControl.defaultProps.maxLength)
-      .toEqual(props.maxLength)
-    expect(PlatePrefixFormControl.defaultProps.onKeyPress)
-      .toBeDefined()
+  it('check if props are set correctly', () => {
+    const onChange = jest.fn(),
+      props = {
+        id: 'platePrefix',
+        value: '',
+        onChange,
+      },
+      DateInputComponent = mount(<PlatePrefixFormControl {...props} />)
+    expect(DateInputComponent.prop('id'))
+      .toEqual('platePrefix')
+    expect(DateInputComponent.prop('value'))
+      .toEqual('')
+    expect(DateInputComponent.prop('onChange'))
+      .toEqual(onChange)
+
   })
 
-})
-
-describe('Testing only-letters acceptance', () => {
-  it('Accepts only letters', () => {
-
+  it('checks prop types', () => {
+    const onChange = jest.fn(),
+      props = {
+        id: 'platePrefix',
+        value: '',
+        onChange,
+      },
+      DateInputComponent = mount(<PlatePrefixFormControl {...props} />)
+    expect(DateInputComponent.prop('id')).toBeString()
+    expect(DateInputComponent.prop('value')).toBeString()
+    expect(DateInputComponent.prop('onChange')).toBeFunction()
+    expect(DateInputComponent.prop('type')).toBeString()
   })
 })
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -99,7 +99,6 @@ export default class Home extends Component {
   }
 
   render () {
-    console.log(this.state)
     const result = this.checkIfCanBeOnRoad()
     return (
       <Panel bsStyle="primary">
